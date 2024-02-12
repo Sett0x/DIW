763d551b0a6febde40aa888c7222dcc4b2ba8cca
José <eltitosetto@gmail.com>
2024-02-12 23:10:28 +0100

    solid init advanced

--- Tailwind/T4 - Replicando un objeto con Tailwind/tailwind.config.js
+++ Tailwind/T4 - Replicando un objeto con Tailwind/tailwind.config.js
@@ -3,8 +3,28 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily:{
+      fontFamily: {
         'Hanken-Grotesk': ['Hanken-Grotesk', 'sans-serif']
+      },
+      colors: {
+        'Lightred': 'hsl(0, 100%, 67%)',
+        'Lightred-alpha': 'hsla(0, 100%, 67%, 0.1)',
+        'Orangeyyellow': 'hsl(39, 100%, 56%)',
+        'Orangeyyellow-alpha': 'hsla(39, 100%, 56%, 0.1)',
+        'Greenteal': 'hsl(166, 100%, 37%)',
+        'Greenteal-alpha': 'hsla(166, 100%, 37%, 0.1)',
+        'Cobaltblue': 'hsl(234, 85%, 45%)',
+        'Cobaltblue-alpha': 'hsla(234, 85%, 45%, 0.1)',
+        'Light-slate-blue': 'hsl(252, 100%, 67%)',
+        'Light-royal-blue': 'hsl(241, 81%, 54%)',
+        'Violet-blue': 'hsla(256, 72%, 46%, 1)',
+        'Persian-blue': 'hsla(241, 72%, 46%, 0)',
+        'White': 'hsl(0, 0%, 100%)',
+        'Pale-blue': 'hsl(221, 100%, 96%)',
+        'Light-lavender': 'hsl(241, 100%, 89%)',
+        'Dark-gray-blue': 'hsl(224, 30%, 27%)',
+        'Dark-gray-blue-alpha': 'hsl(224, 30%, 27%, 0.5)',
+
       }
     },
   },
